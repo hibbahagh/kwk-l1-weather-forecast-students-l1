97e743595157f5c6b00cb58fb1fbeff38556a8b5
hibbahagh <hibbahagh@gmail.com>
2018-06-08 17:42:46 +0000

    automatically backed up by learn

---
+++ new.rb
@@ -0,0 +1,58 @@
+# class Weather 
+#   def intialize(city, temperature, description)
+#     @city = city 
+#     @temperature = temperature
+#     @description = description
+#   end 
+  
+#   def city 
+#     @city 
+#   end 
+  
+#   def temperature
+#     @temperature
+#   end 
+  
+#   def description 
+#     @description
+#   end 
+  
+# end  
+
+# weather_1 = Weather.new("New York","66.3","partly cloudy")
+# weather_2 = Weather.new("Denver", "83", "sunny")
+# weather_3 = Weather.new("Dallas", "100", "humid") 
+
+# puts weather_1.city 
+
+
+
+
+
+
+
+weather = {
+  {"New York" => 66.3}
+  {"Denver" => 83}
+  {"Dallas"=> 100}
+}
+
+puts weather[0][1] 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
